cc212594dd6d14e6c2bad54b14dffaf49d440644
ktho22 <ktho22@kaist.ac.kr>
2016-04-25 10:13:56 +0900

    add inits

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import os
+
+if os.path.isdir("$HOME/.jupyter/profile_nbserver")is False:
+    os.system("ipython profile create nbserver")
+else: 
+    os.system("echo profile_nbserver is already exist.")
+
+yes = set(['yes','y', 'ye', ''])
+no = set(['no','n'])
+
+config_str = """
+# Server config
+c = get_config()
+c.NotebookApp.ip = '*'
+c.NotebookApp.open_browser = False
+c.NotebookApp.port = 8888
+"""
+
+home_dir = os.environ['HOME']
+
+with open(home_dir+"/.jupyter/jupyter_notebook_config.py", "w") as cf:
+	cf.write(config_str)
